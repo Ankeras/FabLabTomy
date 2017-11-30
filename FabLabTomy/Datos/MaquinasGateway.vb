89b4b4ad23e40bd90939dda3ef95c046aebce99a
alumno <alumno@2DAMA22>
2017-11-30 12:51:10 +0100

    Toni: Empezada la clase MaquinasGateway

--- FabLabTomy/Datos/MaquinasGateway.vb
+++ FabLabTomy/Datos/MaquinasGateway.vb
@@ -6,7 +6,25 @@
 ''' </summary>
 Public Class MaquinasGateway
 
-    'objeto con el que abriremos la conexión
+    ''' <summary>
+    ''' Objeto con el que abriremos la conexión
+    ''' </summary>
     Private conexion As SqlConnection
+
+    ''' <summary>
+    ''' Objeto con el que ejecutaremos las consultas
+    ''' </summary>
+    Private comando As SqlCommand
+
+    ''' <summary>
+    ''' Constructor de la clase, crea el acceso a la base de datos
+    ''' </summary>
+    ''' <param name="cadenaConexion">Cadena de conexion de la base de datos</param>
+    Public Sub New(cadenaConexion As String)
+        conexion = New SqlConnection(cadenaConexion)
+        comando = New SqlCommand()
+        comando.Connection = conexion
+    End Sub
+
 
 End Class
