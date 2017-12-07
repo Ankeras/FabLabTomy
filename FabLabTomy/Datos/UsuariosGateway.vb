2567951f1c27f184bc2e54b3340578fc8c58bfc5
OrenjiCo <OrenjiCo@hotmail.com>
2017-12-07 10:25:54 +0100

    GateWayUsuario terminado,faltan algunos retoques

--- FabLabTomy/Datos/UsuariosGateway.vb
+++ FabLabTomy/Datos/UsuariosGateway.vb
@@ -34,16 +34,59 @@ Public Class UsuariosGateway
             Throw New ArgumentException("El teléfono debe contener 9 dígitos")
         End If
 
-
-
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            filas = comando.ExecuteNonQuery()
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If conexion IsNot Nothing Then
+                conexion.Close()
+            End If
+        End Try
         Return filas
     End Function
 
 
 
-    Public Function Actualizar() As Integer
+    Public Function Actualizar(nombre As String, apellido As String, fecha As DateTime, telefono As String, email As String, direccion As String, organizacion As String, tipo As Integer, observaciones As String, id As Integer) As Integer
         Dim filas As Integer
+        Dim consulta As String = String.Format("UPDATE Usuarios SET nombre='{0}',apellido='{1}',fecha_nacimiento='{2}',telefono='{3}',email='{4}',direccion='{5}',organizacion='{6}',tipo='{7}',observaciones='{8}' WHERE id='{10}')", nombre, apellido, fecha, telefono, email, direccion, organizacion, tipo, observaciones, id)
 
+        If id = 0 Or id = Nothing Then
+            Throw New ArgumentException("La id no puede ser 0 ni nulo")
+        End If
+        If nombre = "" Or nombre Is Nothing Then
+            Throw New ArgumentException("El nombre no puede estar vacío")
+        End If
+
+        If apellido = "" Or apellido Is Nothing Then
+            Throw New ArgumentException("El apellido no puede estar vacío")
+        End If
+        If fecha < DateTime.MinValue Or fecha > DateTime.MaxValue Then
+
+        End If
+
+        If telefono = "" Or telefono Is Nothing Then
+            Throw New ArgumentException("El telefono no puede estar vacío")
+        End If
+
+        Dim er As New System.Text.RegularExpressions.Regex("[0-9]{9}")
+        If Not er.IsMatch(telefono) Then
+            Throw New ArgumentException("El teléfono debe contener 9 dígitos")
+        End If
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            filas = comando.ExecuteNonQuery()
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If conexion IsNot Nothing Then
+                conexion.Close()
+            End If
+        End Try
 
         Return filas
     End Function
@@ -52,37 +95,161 @@ Public Class UsuariosGateway
 
     Public Function Eliminar() As Integer
         Dim filas As Integer
-
+        ''''''''''''''''''''''''''''' HACER CONSULTA ''''''''''''''''''''''''''''''
+        Dim consulta As String = ""
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            filas = comando.ExecuteNonQuery()
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
 
         Return filas
     End Function
 
 
     Public Function SelecionarToddos() As DataTable
-        Dim resultado As DataTable
+        Dim consulta As String = String.Format("SELECT * FROM Usuarios")
+        Dim resultado As New DataTable
+        Dim lector As SqlDataReader
+
+
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            lector = comando.ExecuteReader()
+            resultado.Load(lector)
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+        Return resultado
+
+    End Function
+    Public Function SeleccionarId(id As Integer) As DataTable
+
+        Dim consulta As String = String.Format("SELECT * FROM Usuarios WHERE id={0}", id)
+        Dim resultado As New DataTable
+        Dim lector As SqlDataReader
+
+
+        If id = 0 Then
+            Throw New ArgumentException("El identificador no puede estar vacío")
+        End If
+
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            lector = comando.ExecuteReader()
+
+
+            resultado.Load(lector)
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
+
+
+        Return resultado
+
+    End Function
+    Public Function SelecionarApellido(apellido As String) As DataTable
+        Dim consulta As String = String.Format("SELECT * FROM Usuarios WHERE apellido={0}", apellido)
+        Dim resultado As New DataTable
+        Dim lector As SqlDataReader
+
+
+        If apellido = "" Or apellido Is Nothing Then
+            Throw New ArgumentException("El apellido no puede estar vacío o nulo")
+        End If
+
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            lector = comando.ExecuteReader()
+
+
+            resultado.Load(lector)
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
 
 
         Return resultado
     End Function
 
-    Public Function SelecionarApellido() As DataTable
-        Dim resultado As DataTable
+    Public Function SelecionarEdad(edad As Integer) As DataTable
+        Dim consulta As String = String.Format("SELECT * FROM Usuarios WHERE edad={0}", edad)
+        Dim resultado As New DataTable
+        Dim lector As SqlDataReader
+
+
+        If edad = Nothing Then
+            Throw New ArgumentException("La edad no puede ser nulo")
+        End If
+
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            lector = comando.ExecuteReader()
+
+
+            resultado.Load(lector)
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
 
 
         Return resultado
     End Function
 
-    Public Function SelecionarEdad() As DataTable
-        Dim resultado As DataTable
+    ''' <summary>
+    ''' MIRAAAAR COMENTARIO EN BD !
+    ''' </summary>
+    ''' <param name="comentario"></param>
+    ''' <returns></returns>
+    Public Function SelecionarComentario(comentario As String) As DataTable
+        Dim consulta As String = String.Format("SELECT * FROM Usuarios WHERE edad={0}", comentario)
+        Dim resultado As New DataTable
+        Dim lector As SqlDataReader
 
 
-        Return resultado
-    End Function
-    Public Function SelecionarComentario() As DataTable
-        Dim resultado As DataTable
 
 
-        Return resultado
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            lector = comando.ExecuteReader()
+
+
+            resultado.Load(lector)
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+            End If
+        End Try
     End Function
 
     'Punto de acceso a base de datos(FabLabDB)
