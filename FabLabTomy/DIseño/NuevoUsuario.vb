febed79e4acce7ef3c26f63daf2ec533827775e0
OrenjiCo <OrenjiCo@hotmail.com>
2017-12-11 21:17:56 +0100

    Auto cargar datos NuevoUsuario

--- FabLabTomy/DIseño/NuevoUsuario.vb
+++ FabLabTomy/DIseño/NuevoUsuario.vb
@@ -15,8 +15,39 @@
     Private Sub NuevoUsuario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If rol = 1 Then
             Dim resultado As DataTable = NegocioUsuarios.ObtenerUsuarioId(id)
+
             For Each row As DataRow In resultado.Rows
+
                 NombreTextBox.Text = CStr(row("nombre"))
+                ApellidoTextBox.Text = CStr(row("apellidos"))
+                FechaDateTimePicker.Value = CDate(row("fecha_nacimiento"))
+                If row.IsNull("organizacion") Then
+                    OrganizacionTextBox.Text = " "
+                Else
+                    OrganizacionTextBox.Text = CStr(row("organizacion"))
+                End If
+
+                If row.IsNull("direccion") Then
+                    DireccionPostalTextBox.Text = " "
+                Else
+                    DireccionPostalTextBox.Text = CStr(row("direccion"))
+                End If
+
+                If row.IsNull("telefono") Then
+                    TelefonoTextBox.Text = " "
+                Else
+                    TelefonoTextBox.Text = CStr(row("telefono"))
+                End If
+
+                If row.IsNull("email") Then
+                    EmailTextBox.Text = " "
+                Else
+                    EmailTextBox.Text = CStr(row("email"))
+                End If
+
+
+
+
             Next
         End If
     End Sub
