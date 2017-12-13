f394bdf578b063bdb5f878aa1d5606dab699ed0e
Antonio <Antonio@TONI>
2017-12-13 18:23:08 +0100

    Toni: Creados negocios de insertar y actualizar

--- FabLabTomy/Codigo/NegocioMaquinas.vb
+++ FabLabTomy/Codigo/NegocioMaquinas.vb
@@ -6,6 +6,26 @@
         Return gateway.SeleccionarTodasLasMaquinas()
     End Function
 
+    Public Function InsertarMaquina(modelo As String, precioHora As Decimal, fechaCompra As DateTime,
+                             telefonoSAT As String, tipo As Integer, descripcion As String,
+                             caracteristicas As String) As Integer
+
+        Dim gateway As New MaquinasGateway(My.Settings.cadena)
+
+        Return gateway.Insertar(modelo, precioHora, fechaCompra, telefonoSAT, tipo, descripcion, caracteristicas)
+
+    End Function
+
+    Public Function ActualizarMaquina(modelo As String, precioHora As Decimal, fechaCompra As DateTime,
+                             telefonoSAT As String, tipo As Integer, descripcion As String,
+                             caracteristicas As String, id As Integer) As Integer
+
+        Dim gateway As New MaquinasGateway(My.Settings.cadena)
+
+        Return gateway.Actualizar(modelo, precioHora, fechaCompra, telefonoSAT, tipo, descripcion, caracteristicas, id)
+
+    End Function
+
     Public Function BorrarMaquinaId(id As Integer) As Integer
         Dim gateway As New MaquinasGateway(My.Settings.cadena)
 
