e403433241e33dea4e5b8428d07a158235afa87a
OrenjiCo <OrenjiCo@hotmail.com>
2017-12-15 19:00:35 +0100

    API 100%

--- FabLabTomy/Codigo/APIs.vb
+++ FabLabTomy/Codigo/APIs.vb
@@ -1,20 +1,15 @@
 ﻿Module APIs
 
 
-    Public Sub ComputerVisionAPI(ruta As String)
-        'Dim thumbnail = GetThumbnail(ruta)
 
-
-    End Sub
-    Public Async Function GetThumbnail(ByVal path As String) As Task
+    Public Async Function ComputerVisionAPI(ByVal path As String) As Task
         Try
 
             Dim client As New Microsoft.ProjectOxford.Vision.VisionServiceClient("eb00e6ae53e64100b5ffd6d5c43bf619", "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0")
             Dim stream As System.IO.Stream = System.IO.File.OpenRead(path)
             Dim thumbnail As Byte() = Await client.GetThumbnailAsync(stream, 200, 200, True)
-            Dim writer As System.IO.StreamWriter = New IO.StreamWriter(My.Settings.rutaFotosUsuario & "123.jpg")
-            writer.Write(thumbnail)
-            writer.Close()
+            System.IO.File.WriteAllBytes(My.Settings.rutaFotosUsuario & "123.jpg", thumbnail)
+
 
         Catch ex As Exception
             MessageBox.Show(ex.ToString)
